8ef955b8ca77b6e345dc2a03de0b0770a27b9935
Jean Ramilo <jeanlouiseramilo@gmail.com>
2024-08-07 23:42:01 +0800

    Update MealItem to display other meal data
    Previous commit just displays the meal name. This commit displays the
    meal image, price, description, and Add to Cart button.

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import MealItem from './MealItem';
 
 export default function Meals() {
   const [loadedMeals, setLoadedMeals] = useState([]);
@@ -13,7 +14,7 @@ export default function Meals() {
       const meals = await response.json();
       setLoadedMeals(meals);
     }
-    
+
     fetchMeals();
   }, []);
   
@@ -24,7 +25,9 @@ export default function Meals() {
   
   return (
     <ul id="meals">
-      {loadedMeals.map(meal => <li key={meal.id}>{meal.name}</li>)}
+      {loadedMeals.map(meal => 
+        <MealItem key={meal.id} meal={meal} />
+      )}
     </ul>
   )
 }
