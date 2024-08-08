ddbe2e40f1543829b6bf4c254705547f5719eb0f
Jean Ramilo <jeanlouiseramilo@gmail.com>
2024-08-09 03:01:04 +0800

    Use UserProgressContext inside Header
    The cart text-button inside Header needs context on user progress/user actions to determine whether or not to display Cart.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,19 @@ import { useContext } from 'react';
 import Button from './UI/Button';
 import logoImg from '../assets/logo.jpg';
 import CartContext from '../store/CartContext';
+import UserProgressContext from '../store/UserProgressContext';
 
 export default function() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
 
   const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
     return totalNumberOfItems + item.quantity;
   }, 0);
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
 
   return (
     <header id="main-header">
@@ -17,7 +23,7 @@ export default function() {
         <h1>ReactFood</h1>
       </div>
       <nav>
-        <Button textOnly>Cart ({totalCartItems})</Button>
+        <Button textOnly onClick={handleShowCart}>Cart ({totalCartItems})</Button>
       </nav>
     </header>
   )
