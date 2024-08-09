79a574228a963f111b9ec072ed789609b5703549
Jean Ramilo <jeanlouiseramilo@gmail.com>
2024-08-09 15:22:53 +0800

    Create CartItem component and use CartItem in Cart component
    The task of displaying each content of the cart was moved from Cart to
    CartItem. Add and remove item buttons are still not functional.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,6 +4,7 @@ import Button from './UI/Button';
 import CartContext from '../store/CartContext';
 import { currencyFormatter } from '../util/formatting';
 import UserProgressContext from '../store/UserProgressContext';
+import CartItem from './CartItem';
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
@@ -22,9 +23,14 @@ export default function Cart() {
     <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
       <h2>Your Cart</h2>
       <ul>
-        {cartCtx.items.map(item => 
-          <li key={item.id}>{item.name} - {item.quantity}</li>
-        )}
+        {cartCtx.items.map(item => (
+          <CartItem 
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+          />
+        ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
