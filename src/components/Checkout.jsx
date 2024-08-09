2bd0af29b5f95e41a615d20168fd1d475e3d9a61
Jean Ramilo <jeanlouiseramilo@gmail.com>
2024-08-09 21:11:16 +0800

    Update Checkout
    Add input fields for the customer to fill up using Input component. Add
    Close and Submit buttons. Only the Close button is function as of this
    commit.

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,20 +1,49 @@
 import { useContext } from 'react';
 import Modal from './UI/Modal';
-import CartContext from '../components/UI/Modal';
+import CartContext from '../store/CartContext';
+import UserProgressContext  from '../store/UserProgressContext';
 import { currencyFormatter } from '../util/formatting';
+import Input from '../components/UI/Input';
+import Button from './UI/Button';
 
 export default function Checkout() {
   const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
 
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price, 
     0
   );
 
+  function handleClose() {
+    userProgressCtx.hideCheckout();
+  }
+
   return (
-    <form>
-      <h2>Checkout</h2>
-      <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-    </form>
+    <Modal open={userProgressCtx.progress === 'checkout'}>
+      <form>
+        <h2>Checkout</h2>
+        <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
+
+        <Input label="Full Name" type="text" id="full-name"/>
+        <Input label="E-mail Address" type="email" id="email"/>
+        <Input label="Street" type="text" id="street"/>
+        <div className="control-row">
+          <Input label="Postal Code" type="text" id="postal-code" />
+          <Input label="City" type="text" id="city" />
+        </div>
+
+        <p className="modal-actions">
+          <Button 
+            type="button" 
+            onClick={handleClose} 
+            textOnly
+          >
+            Close
+          </Button>
+          <Button>Submit Order</Button>
+        </p>
+      </form>
+    </Modal>
   )
 }
