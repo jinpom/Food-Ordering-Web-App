1e1677052e2b65780868c20661bd23c9b640b72a
Jean Ramilo <jeanlouiseramilo@gmail.com>
2024-08-09 22:20:10 +0800

    Fix bug in Modal component
    Bug: When user presses the Esc key on his/her keyboard, the Cart and Checkout
    Modals could not be opened again because closing the Modal using Esc key does not update the progress context. User progress context only gets updated when the Close button inside the Modal is pressed to dismiss the Modal. The fix is to call back the progress update function when Modal closes: onClose={callbackFxn}.

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-export default function({ children, open, className ='' }) {
+export default function({ children, open, onClose, className ='' }) {
   const dialog = useRef();
 
   useEffect(() => {
@@ -15,7 +15,13 @@ export default function({ children, open, className ='' }) {
   }, [open])
   
   return createPortal(
-    <dialog ref={dialog} className={`modal ${className}`}>{children}</dialog>,
+    <dialog 
+      ref={dialog} 
+      className={`modal ${className}`}
+      onClose={onClose}
+    >
+      {children}
+    </dialog>,
     document.getElementById('modal')
   );
 }
