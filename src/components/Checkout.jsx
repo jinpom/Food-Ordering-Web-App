db1d00b1b5ac0cce14e4c5751c0a2f29dca600e3
Jean Ramilo <jeanlouiseramilo@gmail.com>
2024-08-10 23:19:37 +0800

    Reformat Checkout

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,17 +1,17 @@
-import { useContext } from 'react';
-import Modal from './UI/Modal';
-import CartContext from '../store/CartContext';
-import UserProgressContext  from '../store/UserProgressContext';
-import { currencyFormatter } from '../util/formatting';
-import Input from '../components/UI/Input';
-import Button from './UI/Button';
+import { useContext } from "react";
+import Modal from "./UI/Modal";
+import CartContext from "../store/CartContext";
+import UserProgressContext from "../store/UserProgressContext";
+import { currencyFormatter } from "../util/formatting";
+import Input from "../components/UI/Input";
+import Button from "./UI/Button";
 
 export default function Checkout() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
 
   const cartTotal = cartCtx.items.reduce(
-    (totalPrice, item) => totalPrice + item.quantity * item.price, 
+    (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
   );
 
@@ -23,49 +23,42 @@ export default function Checkout() {
     event.preventDefault();
     const fd = new FormData(event.target);
     const customerData = Object.fromEntries(fd.entries());
-  
-    fetch('http://localhost:3000/orders', {
-      method: 'POST',
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         order: {
           items: cartCtx.items,
-          customer: customerData
-        }
-      })
+          customer: customerData,
+        },
+      }),
     });
   }
 
   return (
-    <Modal 
-      open={userProgressCtx.progress === 'checkout'}
-      onClose={handleClose}
-    >
+    <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
       <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
-        <Input label="Full Name" type="text" id="name"/>
-        <Input label="E-mail Address" type="email" id="email"/>
-        <Input label="Street" type="text" id="street"/>
+        <Input label="Full Name" type="text" id="name" />
+        <Input label="E-mail Address" type="email" id="email" />
+        <Input label="Street" type="text" id="street" />
         <div className="control-row">
           <Input label="Postal Code" type="text" id="postal-code" />
           <Input label="City" type="text" id="city" />
         </div>
 
         <p className="modal-actions">
-          <Button 
-            type="button" 
-            onClick={handleClose} 
-            textOnly
-          >
+          <Button type="button" onClick={handleClose} textOnly>
             Close
           </Button>
           <Button>Submit Order</Button>
         </p>
       </form>
     </Modal>
-  )
+  );
 }
